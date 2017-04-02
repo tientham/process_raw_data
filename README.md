e7bab3105378748380acbbd24e9a4866f7776378
Minh Tien To <tien.tominh@gmail.com>
2017-04-02 13:06:34 +0200

    Converting raw data into readable format (replacing commas with dots)

---
+++ README.md
@@ -0,0 +1,29 @@
+-------------------------------------
+Project: Process raw data using python
+--------------------------------------
+
+Author: Minh Tien TO (tien.tominh@gmail.com)
+
+Description: 
+	This project helps you to read / write raw data inputs.
+	Calculate max, min, average data
+	Plot graph based on data inputs.
+
+--------------
+Prestitiques:
+--------------
+1. matplotlib
+
+
+
+#####################
+How TO:
+- Install matplotlib
+  sudo apt-get install python-matplotlib
+-
+
+#####################
+References:
+- http://python-astro.blogspot.it/2012/02/read-2-columns-file-and-plot-result.html
+- http://stackoverflow.com/questions/11815538/how-to-import-matplotlib-in-python
+- http://stackoverflow.com/questions/24417149/match-float-with-comma-and-then-replace-comma-with-dot
--- process_data.py
+++ process_data.py
@@ -1,14 +1,39 @@
 #!/usr/bin/env python
-# Author: TienTham - Junior Consultant - Altran
-# minhtien.to@altran.com / tien.tominh@gmail.com
+# Author: TienTham
+# tien.tominh@gmail.com
 # 02nd April, 2017 
 
 import matplotlib.pyplot as plt
 import numpy as np
+import re
 
-f2 = open('fakedata/data_1', 'r')
+# Chaning data format if needed
+with open('fakedata/data_2', 'r+') as f:
+	newf= re.sub(r'(\s+[+-]?[0-9]+),([0-9]+\s+)', r'\1.\2', f.read())
+	f.seek(0)
+	f.write(newf)
+
+# Storing data
+f2 = open('fakedata/data_2', 'r')
 lines = f2.readlines()
 f2.close()
-type(lines)
-len(lines)
-print(type(lines[0]))
+#type(lines)
+#len(lines)
+#print(type(lines[0]))
+
+# Initialize some variable to be lists:
+x1= []
+y1= []
+
+# Scan the row of the file stroed in lines and put the values into some variables
+for line in lines:
+	p = line.split()
+	x1.append(float(p[0]))
+	y1.append(float(p[1]))
+
+xv = np.array(x1)
+yv = np.array(y1)
+
+# Plot the data
+plt.plot(xv,yv)
+plt.show()
